5be2a60fff445c3d61af542dea984ec22b818b57
CodyBeyer <codybeyer@Codys-MBP.lan>
2014-11-18 14:13:30 -0800

    FirstCommit

---
+++ FormsToolbar/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FormsToolbar
+{
+	public class App
+	{
+		public static Page GetMainPage()
+		{
+			return new NavigationPage(new ToolBarPage());
+		}
+	}
+}
+
+
---
+++ FormsToolbar/ToolBarPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FormsToolbar
+{	
+	public partial class ToolBarPage : ContentPage
+	{	
+		public ToolBarPage ()
+		{
+			InitializeComponent ();
+		}
+
+		void OnClick(object sender, EventArgs e)
+		{
+			ToolbarItem tbi = (ToolbarItem) sender;
+			this.DisplayAlert("Selected!", tbi.Name,"OK");
+		}
+	}
+}
+
